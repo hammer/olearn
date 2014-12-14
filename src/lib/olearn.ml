cce27fa256513444908dc9e5e70897f290cb031b
Jeff Hammerbacher <jeff.hammerbacher@gmail.com>
2014-12-14 12:54:37 -0500

    Makes non-shuffle code functional.

--- src/lib/olearn.ml
+++ src/lib/olearn.ml
@@ -27,6 +27,14 @@ type hyperparameters = {
   learning_rate : float;
 }
 
+type epoch_state = {
+  m : model;
+  h : hyperparameters;
+}
+
+type fits = epoch_state list
+
+
 (* Thanks, batteries (and Fisher-Yates) *)
 let shuffle a =
   for n = Array.length a - 1 downto 1 do
@@ -45,13 +53,13 @@ let print_models ms =
   List.iter (fun m -> Printf.printf "theta: %f beta: %f\n" m.theta m.beta) ms
 
 (* TODO(hammer): verify these arrays have the same length *)
-let r2_score y y_hat =
-  let y_len = Array.length y in
-  let diff_sq = Array.init y_len (fun i -> (y.(i) -. y_hat.(i)) ** 2.) in
-  let numerator = Array.fold_left (fun x y -> x +. y) 0. diff_sq in
-  let y_avg = (Array.fold_left (fun x y -> x +. y) 0. y) /. (float y_len) in
-  let y_spread_sq = Array.init y_len (fun i -> (y.(i) -. y_avg) ** 2.) in
-  let denominator = Array.fold_left (fun x y -> x +. y) 0. y_spread_sq in
+let r2_score ys y_hats =
+  let pairs = BatEnum.combine ((BatArray.enum ys), (BatArray.enum y_hats)) in
+  let diff_sq = BatArray.of_enum (BatEnum.map (fun (x, y) -> (x -. y) ** 2.) pairs) in
+  let numerator = BatArray.fsum diff_sq in
+  let y_avg = BatArray.favg ys in
+  let y_spread_sq = BatArray.mapi (fun i y -> (y -. y_avg) ** 2.) ys in
+  let denominator = BatArray.fsum y_spread_sq in
   match numerator, denominator with
   | 0.0, 0.0 -> 1.0
   | _, 0.0 -> 0.0
@@ -60,22 +68,28 @@ let r2_score y y_hat =
 let predict m x =
   m.theta *. x +. m.beta
 
-let step_parameters m s h =
+let step_parameters epoch_state s =
+  let m = epoch_state.m in
+  let h = epoch_state.h in
   let y_hat = predict m s.x in
-  { theta = m.theta -. h.learning_rate *. (y_hat -. s.y) *. s.x;
-    beta = m.beta -. h.learning_rate *. (y_hat -. s.y)
+  {
+    m = { theta = m.theta -. h.learning_rate *. (y_hat -. s.y) *. s.x;
+          beta = m.beta -. h.learning_rate *. (y_hat -. s.y)
+        };
+    h = h;
   }
 
-let single_pass m samples h =
-  let f ms s = (step_parameters (List.hd ms) s h) :: ms in
-  Array.fold_left f m samples
+let single_pass fits samples =
+  let f fits s = (step_parameters (List.hd fits) s) :: fits in
+  Array.fold_left f fits samples
+
+let rec all_passes remaining_epochs fits samples =
+  match remaining_epochs with
+  | 0 -> fits
+  | _ -> all_passes (remaining_epochs - 1) (single_pass fits samples) samples
 
 let fit_regressor samples h =
   let m = { theta = 0.0; beta = 0.0; } in
-  let ms = Array.make (h.epochs + 1) [] in
-  ms.(0) <- [m];
+  let epoch_0 = { m = m; h = h } in
   let shuffled_samples = shuffle samples in
-  for i = 1 to h.epochs do
-    ms.(i) <- single_pass ms.(i-1) shuffled_samples h
-  done;
-  ms
+  all_passes h.epochs [epoch_0] shuffled_samples
--- src/test/test_olearn.ml
+++ src/test/test_olearn.ml
@@ -15,26 +15,26 @@
 open OUnit2
 open Olearn
 
-let test_score test_ctxt =
+let test_predict test_ctxt =
   let m = { theta = 1.0; beta = 1.0; } in
   assert_equal 2.0 (predict m 1.0)
 
 let test_simple_regression test_ctxt =
   let h = { learning_rate = 0.01; epochs = 20; } in
-  let x = Array.init 100 (fun _ -> 5.0 -. Random.float 10.0) in
-  let y = Array.init 100 (fun i -> 0.5 *. x.(i)) in
-  let make_sample (x, y) = { x = x; y = y } in
-  let samples = Array.init 100 (fun i -> make_sample (x.(i), y.(i))) in
-  let ms = fit_regressor samples h in
-  let fitted_model = List.hd ms.(h.epochs) in
-  let y_hat = Array.init 100 (fun i -> predict fitted_model x.(i)) in
-  let score = r2_score y y_hat in
+  let xs = Array.init 100 (fun _ -> 5.0 -. Random.float 10.0) in
+  let ys = Array.map (fun x -> 0.5 *. x) xs in
+  let make_sample x y = { x = x; y = y } in
+  let samples = BatArray.map2 make_sample xs ys in
+  let fits = fit_regressor samples h in
+  let fitted_model = (List.hd fits).m in
+  let y_hats = Array.map (fun x -> predict fitted_model x) xs in
+  let score = r2_score ys y_hats in
   assert_bool "Bad model quality" (score > 0.99)
 
 let suite =
   "suite" >:::
-    [ "test_score" >:: test_score;
-      "test_simple_regression" >:: test_score;
+    [ "test_predict" >:: test_predict;
+      "test_simple_regression" >:: test_simple_regression;
     ]
 
 let () =
